d3fcf5ac6ccd52ac0e2ce99d8f24e670ca6a02bc
Nikita <ns_stepanov@inbox.ru>
2024-03-09 13:39:13 +0300

    add jwt sign

---
+++ backend/internal/pkg/dto/token-result.go
@@ -0,0 +1,5 @@
+package dto
+
+type TokenResult struct {
+	Token string
+}
--- backend/internal/pkg/services/account.go
+++ backend/internal/pkg/services/account.go
@@ -2,12 +2,15 @@ package services
 
 import (
 	"net/http"
+	"time"
 
-	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/storage/entities"
-	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/storage"
-	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/dto"
-	"golang.org/x/crypto/bcrypt"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/dto"
+	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/storage"
+	"github.com/nikitaSstepanov/p2p-streaming-service/backend/internal/pkg/storage/entities"
+	"github.com/spf13/viper"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type Account struct {
@@ -24,7 +27,7 @@ func (a *Account) Create(ctx *gin.Context) {
 	var dto dto.CreateUserDto
 
 	if err := ctx.ShouldBindJSON(&dto); err != nil {
-		return 
+		return
 	}
 
 	candidate := a.Storage.Users.GetUser(ctx, dto.Username)
@@ -49,7 +52,35 @@ func (a *Account) Create(ctx *gin.Context) {
 }
 
 func (a *Account) SignIn(ctx *gin.Context) {
+	var body dto.SignInDto
 
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		return
+	}
+
+	user := a.Storage.Users.GetUser(ctx, body.Username)
+
+	if user.Id == 0 {
+		return
+	}
+
+	err := checkPassword(user.Password, body.Password)
+
+	if err != nil {
+		return
+	}
+
+	token, err := generateToken(user.Username)
+
+	if err != nil {
+		return
+	}
+
+	result := &dto.TokenResult{
+		Token: token,
+	}
+
+	ctx.JSON(200, result)
 }
 
 func hashPassword(password string) (string, error) {
@@ -60,4 +91,33 @@ func hashPassword(password string) (string, error) {
 	}
 
 	return string(hash), nil
+}
+
+func checkPassword(hash string, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func generateToken(username string) (string, error) {
+	claims := jwt.MapClaims{
+		"exp": time.Now().Add(1 * time.Hour),
+		"username": username,
+	}
+
+	key := viper.GetString("jwt.key")
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(key))
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
